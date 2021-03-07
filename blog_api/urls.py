8f057f924eec522a3ef4df683986197a02aeff32
velikiiadmin <73746565+velikiiadmin@users.noreply.github.com>
2021-03-07 20:00:18 +0200

    123

--- blog_api/urls.py
+++ blog_api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from .views import *
 
-# app_name = 'blog_api'
+app_name = 'blog_api'
 
-# urlpatterns = [
-#     path('<int:pk>/', PostDetail.as_view(), name="detailcreate"),
-#     path('', PostList.as_view(), name='listcreate')
-# ]
+urlpatterns = [
+    path('<int:pk>/', PostDetail.as_view(), name="detailcreate"),
+    path('', PostList.as_view(), name='listcreate')
+]
