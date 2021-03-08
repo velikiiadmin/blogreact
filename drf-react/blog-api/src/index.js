b087322d3db74b0eee4d073e6c543b39b2c2dc70
andreisavc <73746565+velikiiadmin@users.noreply.github.com>
2021-03-08 07:31:45 +0200

    123

--- drf-react/blog-api/src/index.js
+++ drf-react/blog-api/src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
-const routing = (
+const routing = props => (
     <Router>
         <React.StrictMode>
             <Header />
@@ -18,3 +18,10 @@ const routing = (
         </React.StrictMode>
     </Router>
 );
+
+ReactDOM.render(routing, document.getElementById("root"));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
